0a57c929d088f8996bb22a41b2fe38287e51dff9
Allison _ <allimacq@gmail.com>
2021-03-10 00:47:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello World"
+  end
 
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/team' do
+    
+    @name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    
+    
+    erb :team
+  end
 
 end
